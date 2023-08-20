0fa2ac38af5135f5388b3bda18bef17a3458f092
thtshajjad <shagors@users.noreply.github.com>
2023-08-20 08:51:16 +0600

    file name and size validation code add and design it.. test it works properly

--- server/src/middlewares/uploadFile.js
+++ server/src/middlewares/uploadFile.js
@@ -1,9 +1,14 @@
 const multer = require("multer");
 const path = require("path");
+const createError = require("http-errors");
 
 const UPLOAD_DIR = process.env.UPLOAD_FILE || "public/images/users";
-const MAX_FILE_SIZE = process.env.MAX_FILE_SIZE || 2097152;
-const ALLOWED_FILE_TYPES = process.env.ALLOWED_FILE_TYPES || [];
+const MAX_FILE_SIZE = Number(process.env.MAX_FILE_SIZE) || 2097152;
+const ALLOWED_FILE_TYPES = process.env.ALLOWED_FILE_TYPES || [
+  "jpg",
+  "jpeg",
+  "png",
+];
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -18,6 +23,18 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const fileFilter = (req, file, cb) => {
+  const extname = path.extname(file.originalname);
+  if (!ALLOWED_FILE_TYPES.includes(extname.substring(1))) {
+    return cb(createError(400, "File type is not valid"));
+  }
+  cb(null, true);
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: MAX_FILE_SIZE },
+  fileFilter,
+});
 
 module.exports = upload;
